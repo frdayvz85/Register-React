91090dcc178e63b6a1f8edf6dbe35ef02f664755
Ferid Eyvazov <ferideyvazov25@gmail.com>
2020-03-25 22:23:11 +0400

    first commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import UserForm from './components/UserForm'
+
+
+function App() {
+  return (
+    <div className="App">
+   <div className="container">
+      <UserForm/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/components/Success.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Success extends Component {
+    render() {
+        return (
+            <div className="form-container">
+                <h1 className="mb-5">Thank You for Your Registration</h1>
+            </div>
+        )
+    }
+}
+
+export default Success;
